dce5c831460ef5092151edb6f7ff35982b9e5218
David Carr <dave@daveismyname.com>
2014-02-01 21:24:15 +0000

    Updated comments to be more descriptive

--- activate.php
+++ activate.php
@@ -1,16 +1,21 @@
 <?php
 require('includes/config.php');
 
+//collect values from the url
 $memberID = trim($_GET['x']);
 $active = trim($_GET['y']);
 
+//if id is number and the active token is not empty carry on
 if(is_numeric($memberID) && !empty($active)){
+
 	//update users record set the active column to Yes where the memberID and active value match the ones provided in the array
 	$stmt = $db->prepare("UPDATE members SET active = 'Yes' WHERE memberID = :memberID AND active = :active");
 	$stmt->execute(array(
 		':memberID' => $memberID,
 		':active' => $active
 	));
+
+	//if the row was updated redirect the user
 	if($stmt->rowCount() == 1){
 
 		//redirect to login page
