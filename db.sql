9d8cfde1dfa570023950188ae5393cd011f93be6
David Carr <dave@daveismyname.com>
2015-07-18 11:02:41 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `members` (
   `memberID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
-  `password` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `active` varchar(255) NOT NULL,
   `resetToken` varchar(255) DEFAULT NULL,
