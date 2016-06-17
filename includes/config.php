3ff56bf3391fb07c76330659a76df5d1a0c18070
BinaryEvolved <me@binaryevolved.com>
2016-06-17 05:03:42 -0400

    Fixed a mistake in the PDO Connection String

--- includes/config.php
+++ includes/config.php
@@ -18,9 +18,9 @@ define('SITEEMAIL','noreply@domain.com');
 try {
 
 	//create PDO connection
-	$db = new PDO("mysql:host=".DBHOST."charset=utf8mb4;dbname=".DBNAME, DBUSER, DBPASS);
+	$db = new PDO("mysql:host=".DBHOST.";charset=utf8mb4;dbname=".DBNAME, DBUSER, DBPASS);
     //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);//Suggested to uncomment on production websites
-    $sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Suggested to comment on production websites
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Suggested to comment on production websites
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 } catch(PDOException $e) {
