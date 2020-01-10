f541495085c51bdfc8ea83a6db3fccde31847782
David Carr <dave@daveismyname.com>
2020-01-10 07:36:14 +0000

    only run action when it exists.

--- resetPassword.php
+++ resetPassword.php
@@ -94,14 +94,17 @@ require('layout/header.php');
 						}
 					}
 
-					//check the action
-					switch ($_GET['action']) {
-						case 'active':
-							echo "<h2 class='bg-success'>Your account is now active you may now log in.</h2>";
-							break;
-						case 'reset':
-							echo "<h2 class='bg-success'>Please check your inbox for a reset link.</h2>";
-							break;
+					if (isset($_GET['action'])) {
+
+						//check the action
+						switch ($_GET['action']) {
+							case 'active':
+								echo "<h2 class='bg-success'>Your account is now active you may now log in.</h2>";
+								break;
+							case 'reset':
+								echo "<h2 class='bg-success'>Please check your inbox for a reset link.</h2>";
+								break;
+						}
 					}
 					?>
 
