a0182195c8bb70deb121f90c4cb1d2c14d4f62dc
David Carr <dave@daveismyname.com>
2020-02-11 06:55:36 +0000

    corrected typo

--- memberpage.php
+++ memberpage.php
@@ -1,7 +1,7 @@
 <?php require('includes/config.php'); 
 
 //if not logged in redirect to login page
-if (! $user->is_logged_in()){ {
+if (! $user->is_logged_in()){
     header('Location: login.php'); 
     exit(); 
 }
